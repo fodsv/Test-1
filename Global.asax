84b2d28826f0eb58a82087d9d2785c8e1734ba18
fredrikodin <fodsv97@hotmail.com>
2020-08-31 15:54:10 +0200

    init identity

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="testowinii.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testowinii.Startup))]
+namespace testowinii
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
